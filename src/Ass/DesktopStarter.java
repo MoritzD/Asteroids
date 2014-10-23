155a86aac0592c2e03643fdbef0326cd9aabce2c
Alexander <alexander.eyjolfsson.001@student.uni.lu>
2014-10-23 13:06:17 +0000

    Derp

--- src/Ass/DesktopStarter.java
+++ src/Ass/DesktopStarter.java
@@ -2,10 +2,8 @@ package Ass;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 
-public class DesktopStarter
-{
-	public static void main(String[] args)
-	{
-		new LwjglApplication(new Assignment3Base(), "Assignment 3 Base", 1280, 720, false);
-	}
+public class DesktopStarter {
+    public static void main(String[] args) {
+        new LwjglApplication(new Assignment3Base(), "Lab-arinth", 1280, 800, false);
+    }
 }
